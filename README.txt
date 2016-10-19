50cb1aab6ae30193d777871c441feaf640545bb5
Akshay Sonawane <akshay.vs9543@gmail.com>
2016-10-19 05:43:37 -0400

    blueprint-2

--- README.txt
+++ README.txt
@@ -1,3 +1,9 @@
 ASSUMPTIONS:
 
 1. the input file will have requests in a sequential order  ie R1,R2,R3,....Rn
+
+
+
+to start execution in terminal :
+node fileReader.js data.txt
+
--- fileReader.js
+++ fileReader.js
@@ -9,20 +9,21 @@ var parameter = process.argv[2];
 
 console.log('PARAMETER PASSED IS ' + parameter);
 
-fs.readFile(fileName, function(err, f){
+fs.readFile(fileName, function (err, f) {
 
-    var data= f.toString();
+    var data = f.toString();
 
-    var filteredData= data.split('\n' );
-    var array =  filteredData;
+    var filteredData = data.split('\n');
+    var array = filteredData;
     console.log(array);
-    var arrayTwo=[]
+    var arrayTwo = []
 
-    array.forEach(function(element){
-        if(element){ // handling empty string . ie multiple enters at the end of the file
-            var filteredData2= element.split(' ')[1];
-             arrayTwo.push(filteredData2);
+    array.forEach(function (element) {
+        if (element) { // handling empty string . ie multiple enters at the end of the file
+            var filteredData2 = element.split(' ').map(Number)[1];
+            arrayTwo.push(filteredData2);
         }
     });
     console.log(arrayTwo);
+    console.log(typeof (arrayTwo[1]));
 });
