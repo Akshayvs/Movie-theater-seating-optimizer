22135c2861bffc4ac83acdcae79105d3ab01badd
Akshay Sonawane <akshay.vs9543@gmail.com>
2016-10-23 06:06:55 -0400

    code running logically correct - write integration tests

--- fileReader.js
+++ fileReader.js
@@ -11,10 +11,11 @@ var inputSanitizer = require('./lib/inputSanitizer.js');
 var absolutePath = process.argv[2];
 var relativePath = path.relative('/Users/asonawane/WebstormProjects/traversal', absolutePath);
 
-fs.readFile(relativePath, function (err, f) {
+fs.readFile(relativePath, function(err, f) {
     if (err) {
         console.log('ABORT! - error in reading file from the path given : ' + err);
+    } else {
+        inputSanitizer(f);
     }
-    else inputSanitizer(f);
 
 });
